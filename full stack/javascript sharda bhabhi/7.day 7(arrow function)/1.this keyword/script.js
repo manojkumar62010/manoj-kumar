0cb60844c19f058aef57ab5188bd9f0fc141b7af
manojkumar62010 <114022994+manojkumar62010@users.noreply.github.com>
2023-10-25 16:19:45 +0530

    assignment

--- full stack/javascript sharda bhabhi/7.day 7(arrow function)/1.this keyword/script.js
+++ full stack/javascript sharda bhabhi/7.day 7(arrow function)/1.this keyword/script.js
@@ -5,9 +5,21 @@ const student={
     eng:56,
     math:100,
     phy:67,
-    getAvg(){
+    getAvg(){ //method
         let avg=(this.eng + this.math + this.phy)/3
         console.log(avg)
     }
 
 }
+// Try and catch -: The try statement allows you to define a block of code to be tested for errors while it is being executed.
+// The catch statement allows you to define a block of code to be executed ,if an error occurs in the  try block.
+console.log("helo")
+// let a=5;
+try{
+    console.log(a);
+}
+catch{
+console.log("caught an error ... a is not defined")
+
+}
+console.log("manoj")
